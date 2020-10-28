2684c21db6e47f3afa4b2f86922f1fa377f72ea5
power <446322810@qq.com>
2020-10-28 15:34:57 +0800

    第3次上传git

--- testing/test_calu.py
+++ testing/test_calu.py
@@ -11,33 +11,35 @@ import pytest
 class Testcalc:
     def setup_class(self):
         self.calc = Calculator()
+        print('setup_class')
+
+    def teardown_class(self):
+        print('teardown_class')
+
+    def setup(self):
+        print('计算开始')
 
     def teardown(self):
-        pass
+        print('计算结束')
 
     @pytest.mark.parametrize('a,b,c', [
         (1, 1, 2),
-        (2, 2, 4)
-    ], ids=['test1', 'test2'])
+        (200, 200, 400),
+        (0.1, 0.1, 0.2)
+    ], ids=['xiao', 'da', 'fudianshu'])
     def test_add(self, a, b, c):
         # calc = Calculator()
         result = self.calc.add(a, b)
         assert result == c
 
-    def test_add1(self):
-        # calc = Calculator()
-        result = self.calc.add(1, 1)
-        assert result == 2
-
-    def test_add2(self):
-        # calc = Calculator()
-        result = self.calc.add(1, 1)
-        assert result == 2
-
-    def test_add3(self):
-        # calc = Calculator()
-        result = self.calc.add(1, 1)
-        assert result == 2
+    @pytest.mark.parametrize('a,b,c', [
+        (1, 1, 0),
+        (200, 200, 0),
+        (300, 100, 200)
+    ], ids=('1-1', '200-200', '300-100'))
+    def test_sub(self, a, b, c):
+        result = self.calc.sub(a, b)
+        assert result == c
 
 
 if __name__ == '__main__':
